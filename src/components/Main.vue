2dbb73b96e415a54d532af14132051ee802af6ea
kaisawind <wind.kaisa@gmail.com>
2019-06-24 16:44:18 +0800

    add some git

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="el-container">
     <div class="el-header">
-      Hello World!
+      <div class="site">
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://gitee.com/">码云Gitee</el-link>
+        <el-link class="title" target="_blank" href="https://code.aliyun.com">阿里云Git</el-link>
+        <el-link class="title" target="_blank" href="https://gitlab.com">Gitlab</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+        <el-link class="title" target="_blank" href="https://github.com">Github</el-link>
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +29,24 @@ export default {
 
 <style scoped>
 .el-container {
+  margin-top: 10px;
   min-width: 1190px;
+}
+.site {
+  min-width: 1190px;
+  position: relative;
+  height: 28px;
+  margin: 0 auto 10px;
+  overflow: hidden;
+  background: #fff;
+  border: 1px solid #dee3eb;
+}
+.title {
+  margin-left: 10px;
+  text-align: right;
+  display: inline-block;
+  margin: 4px 37px 4px 0;
+  line-height: 20px;
+  vertical-align: top;
 }
 </style>
